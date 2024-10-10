982ddead24bdaee61ec98b4ff74b05d2054eeb72
Joseph <poedude5229@gmail.com>
2024-10-10 16:49:08 -0400

    Array method starting practice

---
+++ JavaScript/range.js
@@ -0,0 +1,10 @@
+function range(min, max) {
+  let arr1 = [];
+  for (let i = min; i <= max; i++) {
+    arr1.push(i);
+  }
+  return arr1;
+}
+
+console.log(range(3, 10));
+console.log(range(217, 220));
