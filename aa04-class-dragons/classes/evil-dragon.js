719ee63b845eed48cb160aee1dd6a5967c90cf4e
Joseph <poedude5229@gmail.com>
2025-01-04 21:55:28 -0500

    EvilDragon started

--- aa04-class-dragons/classes/evil-dragon.js
+++ aa04-class-dragons/classes/evil-dragon.js
@@ -1,1 +1,12 @@
-// Your code here 
+// Your code here
+const Dragon = require("./dragon");
+
+class EvilDragon extends Dragon {
+  constructor(name, color, evilDoings, nemesis) {
+    super(name, color);
+    this.evilDoings = evilDoings;
+    this.nemesis = nemesis;
+  }
+}
+
+module.exports = EvilDragon;
