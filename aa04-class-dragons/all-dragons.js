ca563f607c2fe1bf152aed7e7f2e0feb9c711d6e
Joseph <poedude5229@gmail.com>
2025-01-04 22:00:25 -0500

    all dragons started

--- aa04-class-dragons/all-dragons.js
+++ aa04-class-dragons/all-dragons.js
@@ -1,12 +1,24 @@
-// Your code here 
+const EvilDragon = require("./classes/evil-dragon");
+const FriendlyDragon = require("./classes/friendly-dragon");
+
+falkor = new FriendlyDragon(
+  "Falkor",
+  "white",
+  [
+    "save Atreyu from the swamp",
+    "save Atreyu from the Nothing",
+    "scare the local bullies into a dumpster",
+  ],
+  "Bastian"
+);
 
 
 /****************************************************************************/
 /******************* DO NOT EDIT CODE BELOW THIS LINE ***********************/
 
 try {
-  module.exports.falkor = falkor
-  module.exports.smaug = smaug
+  module.exports.falkor = falkor;
+  // module.exports.smaug = smaug;
 } catch {
   module.exports = null;
 }
