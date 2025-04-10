222819f95e3012f24a70c35555ad346e5c7f38db
Joseph <poedude5229@gmail.com>
2025-04-10 17:11:53 -0400

    o(1) isPalindrome.js

---
+++ blind75/isPalindrome.js
@@ -0,0 +1,27 @@
+const isPalindrome = (s) => {
+  let betterS = "";
+  for (let i = 0; i < s.length; i++) {
+    if ("ABCDEFGHIJKLMNOPQRSTUVWXYZ".includes(s[i].toUpperCase())) {
+      betterS += s[i].toUpperCase();
+    }
+  }
+  //   console.log(betterS);
+
+  let reversedS = "";
+  for (let i = betterS.length - 1; i >= 0; i--) {
+    reversedS += betterS[i];
+  }
+
+  if (betterS.length !== reversedS.length) {
+    return false;
+  }
+  for (let i = 0; i < betterS.length; i++) {
+    if (betterS[i] !== reversedS[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+console.log(isPalindrome("raceca r?"));
+console.log(isPalindrome("farts?"));
