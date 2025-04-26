4ffc3cf591d58307552ca531db36e65f1ce285d1
Joseph <poedude5229@gmail.com>
2025-04-26 12:37:11 -0400

    Relearning BFS DFS part 2

--- js2/dfs.js
+++ js2/dfs.js
@@ -5,4 +5,22 @@ const B = new TreeNode("B");
 const C = new TreeNode("C");
 const D = new TreeNode("D");
 const E = new TreeNode("E");
-const F = new TreeNode("F")
+const F = new TreeNode("F");
+
+A.addChild(B);
+A.addChild(C);
+A.addChild(D);
+C.addChild(E);
+D.addChild(F);
+
+/*
+Tree Structure:
+      A
+    / | \
+   B  C  D
+      |   \
+      E    F
+
+*/
+
+
