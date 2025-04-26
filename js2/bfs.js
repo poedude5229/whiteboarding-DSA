592050ba79494b6f01aef6487463df68206419af
Joseph <poedude5229@gmail.com>
2025-04-26 13:14:05 -0400

    Relearning BFS DFS part 4

--- js2/bfs.js
+++ js2/bfs.js
@@ -1,1 +1,16 @@
+const TreeNode = require("./treeNodeClass");
 
+const bfs = (node) => {
+  const queue = [node];
+  const visited = new Set();
+  while (queue.length > 0) {
+    let currentNode = queue.pop();
+    if (visited.has(currentNode)) continue;
+    visited.add(currentNode.value);
+    console.log(currentNode.value);
+
+    for (let child of currentNode.children) {
+      if (!visited.has(child.value)) queue.unshift(child);
+    }
+  }
+};
