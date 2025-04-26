78a5358a32b2ff703aabad376e40c35e5f4a5083
Joseph <poedude5229@gmail.com>
2025-04-26 12:34:39 -0400

    Relearning BFS DFS part 1

---
+++ js2/dfs.js
@@ -0,0 +1,8 @@
+import TreeNode from "./treeNodeClass";
+
+const A = new TreeNode("A");
+const B = new TreeNode("B");
+const C = new TreeNode("C");
+const D = new TreeNode("D");
+const E = new TreeNode("E");
+const F = new TreeNode("F")
---
+++ js2/treeNodeClass.js
@@ -0,0 +1,10 @@
+export default class TreeNode {
+  constructor(value) {
+    this.value = value;
+    this.children = [];
+  }
+
+  addChild(child) {
+    this.children.push(child);
+  }
+}
