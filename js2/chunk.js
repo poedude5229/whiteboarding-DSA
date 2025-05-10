1b41abd112e3dc5a32fa7408cb322f93e5bb959c
Joseph <poedude5229@gmail.com>
2025-05-09 22:02:20 -0400

    chunk.js

---
+++ js2/chunk.js
@@ -0,0 +1,10 @@
+const chunk = (arr, size) => {
+  let res = [];
+  for (let i = 0; i < arr.length; i += size) {
+    res.push(arr.slice(i, i + size));
+  }
+  return res;
+};
+
+console.log(chunk([1, 2, 3, 4, 5], 2));
+// ➞ [[1, 2], [3, 4], [5]]
