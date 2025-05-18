ff51d1abe73a9a65f8b31bafaa6e9a6a8c94694f
Joseph <poedude5229@gmail.com>
2025-05-17 21:39:59 -0400

    starting rotate.js

---
+++ js2/rotate.js
@@ -0,0 +1,10 @@
+/*
+You want to rotate an array by a certain number of positions, moving elements from the end to the front.
+
+Given:
+
+rotate([1, 2, 3, 4, 5], 2)
+You want the last 2 elements ([4, 5]) to rotate to the front:
+[4, 5, 1, 2, 3]
+*/
+
