8f11a21bfe176b7e4c51e8034242587ae13727d2
Joseph <poedude5229@gmail.com>
2025-05-17 21:43:25 -0400

    P1 rotate.js

--- js2/rotate.js
+++ js2/rotate.js
@@ -7,4 +7,14 @@ rotate([1, 2, 3, 4, 5], 2)
 You want the last 2 elements ([4, 5]) to rotate to the front:
 [4, 5, 1, 2, 3]
 */
+//norm = 2 % 5 = 2
+// arr.slice(-2)        → [4, 5]
+// arr.slice(0, 3)      → [1, 2, 3]
+// concat → [4, 5, 1, 2, 3]
 
+const rotate = (arr, n) => {
+  const len = arr.length;
+  const normalizedN = n % len;
+
+  return arr.slice(-normalizedN).concat(arr.slice(0, len - norm));
+};
